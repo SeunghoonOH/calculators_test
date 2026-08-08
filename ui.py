da5e10b9100410b5ab9d645262c9ee41f24edd32
SeunghoonOH <intoosh@naver.com>
2026-08-08 13:32:37 +0900

    add power operator to combobox

--- ui.py
+++ ui.py
@@ -23,7 +23,7 @@ class View(QWidget):
         self.le2.setAlignment(QtCore.Qt.AlignRight)
         
         self.cb = QComboBox(self)
-        self.cb.addItems(['+', '-', '*', '/'])
+        self.cb.addItems(['+', '-', '*', '/', '^'])
         
         hbox_formular = QHBoxLayout()
         hbox_formular.addWidget(self.le1)
@@ -59,3 +59,5 @@ class View(QWidget):
         
     def clearMesage(self):
         self.te1.clear()
+        
+    
